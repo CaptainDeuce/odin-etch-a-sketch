0a7b5b80905a925c19aeb8d0087ecf48aac47915
Ethan Boes <deuceboes@gmail.com>
2025-07-13 17:12:38 -0400

    Move for loop into a new function createGrid

--- javascript.js
+++ javascript.js
@@ -1,17 +1,21 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    div.style.border = " 2px solid black";
-    div.style.padding = "51px";
-    container.appendChild(div);
+function createGrid (sqaureNumber) {
+    for (let i = 0; i < squareNumber; i++) {
+        let div = document.createElement("div");
+        div.style.border = " 2px solid black";
+        div.style.padding = "51px";
+        container.appendChild(div);
 
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "purple";
-    });
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "purple";
+        });
+    }
 }
 
 let btn = document.querySelector("button");
 btn.addEventListener("click", () => {
-    prompt("How many squares per side for the grid? (Max 100)");
+    let square = prompt("How many squares per side for the grid? (Max 100)");
+    let squareNumber = Number(square);
+    createGrid(squareNumber);
 });
