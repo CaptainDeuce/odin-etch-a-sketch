13d6389b1b89bf61e1eb1f7df64948c8c134c65a
Ethan Boes <deuceboes@gmail.com>
2025-07-13 16:35:24 -0400

    Add size to each div border

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
-    div.style.border = "solid black";
+    div.style.border = " 2px solid black";
     div.style.padding = "51px";
     container.appendChild(div);
 }
