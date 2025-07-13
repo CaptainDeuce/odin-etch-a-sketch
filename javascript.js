a9e2c80b8b10eafefb1c9e4acac4b9ada423bb09
Ethan Boes <deuceboes@gmail.com>
2025-07-13 16:33:05 -0400

    Add padding to each div

--- javascript.js
+++ javascript.js
@@ -3,5 +3,6 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.style.border = "solid black";
+    div.style.padding = "51px";
     container.appendChild(div);
 }
